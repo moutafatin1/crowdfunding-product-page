fb0ac3105e5f2257dccaa702fd52d92bfc66d3e1
Oussama Moutafatin <moutafatin3@gmail.com>
2022-12-21 08:42:45 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="text-center text-7xl text-purple-500">App</div>;
+};
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        commissioner: ["Commissioner", "sans-serif"],
+      },
+      colors: {
+        crowd: {
+          moderateCyan: "hsl(176, 50%, 47%)",
+          darkCyan: "hsl(176, 72%, 28%)",
+          darkGray: "hsl(0, 0%, 48%)",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
