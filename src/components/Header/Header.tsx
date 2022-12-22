67e37e50845770d7f75a98e472581735c20dced9
Oussama Moutafatin <moutafatin3@gmail.com>
2022-12-22 08:25:14 +0100

    header responsiveness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,8 +14,34 @@ export const Header = () => {
   };
   return (
     <header className="h-72  bg-hero-mobile bg-cover bg-no-repeat p-6">
-      <div className="flex items-center justify-between">
+      <div className="mx-auto flex max-w-6xl items-center justify-between">
         <img src={Logo} alt="logo" />
+        <ul className="hidden items-center gap-6 text-white lg:flex">
+          <li>
+            <a
+              href="#"
+              className="border-b border-transparent font-medium transition-all hover:border-white hover:opacity-70"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="border-b border-transparent font-medium transition-all hover:border-white hover:opacity-70"
+            >
+              Discover
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="border-b border-transparent font-medium transition-all hover:border-white hover:opacity-70"
+            >
+              Get Started
+            </a>
+          </li>
+        </ul>
         <MenuButton isOpen={isOpen} open={open} close={close} />
       </div>
       {isOpen && <MobileMenu />}
