d9c0e277a1b746010768e5364db976a66855a13d
Oussama Moutafatin <moutafatin3@gmail.com>
2022-12-22 09:26:22 +0100

    stats  and progress bar state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import { AboutCard } from "./components/AboutCard";
 import { BackProjectCard } from "./components/BackProject/BackProjectCard";
 import { Header } from "./components/Header/Header";
 import { StatsCard } from "./components/StatsCard";
+export type Crowd = {
+  totalBacked: number;
+  totalBackers: number;
+};
 
 const App = () => {
+  const [crowd, setCrowd] = useState<Crowd>({
+    totalBacked: 89914,
+    totalBackers: 5007,
+  });
+  const backingProgress = (crowd.totalBacked / 100000) * 100;
   return (
     <>
       <Header />
       <main className="font-commissioner">
         <BackProjectCard />
-        <StatsCard />
+        <StatsCard crowd={crowd} backingProgress={backingProgress} />
         <AboutCard />
       </main>
     </>
--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -1,40 +1,41 @@
+import { Crowd } from "../App";
 import { Card } from "./common/Card";
 
-const stats = {
-  totalBacked: 89.914,
-  totalBackers: 5007,
-  daysLeft: 56,
+type StatsCardProps = {
+  crowd: Crowd;
+  backingProgress: number;
 };
 
-export const StatsCard = () => {
+export const StatsCard = ({ crowd, backingProgress }: StatsCardProps) => {
   return (
     <Card className="mt-52 mb-4 flex flex-col items-center gap-8 rounded-xl border border-gray-100 bg-white p-8 lg:mt-60 ">
       <div className="flex flex-col items-center gap-8 lg:flex-row">
         <div className="flex flex-col items-center">
           <span className="text-3xl font-bold text-gray-900">
-            ${stats.totalBacked}
+            ${crowd.totalBacked}
           </span>
           <span className="text-crowd-darkGray">of $100,000 backed</span>
         </div>
         <hr className="w-20 border-gray-300 lg:rotate-90" />
         <div className="flex flex-col items-center">
           <span className="text-3xl font-bold text-gray-900">
-            {stats.totalBackers}
+            {crowd.totalBackers}
           </span>
           <span className="text-crowd-darkGray">total backers</span>
         </div>
         <hr className="w-20 border-gray-300 lg:rotate-90" />
 
         <div className="flex flex-col items-center">
-          <span className="text-3xl font-bold text-gray-900">
-            {stats.daysLeft}
-          </span>
+          <span className="text-3xl font-bold text-gray-900">{56}</span>
           <span className="text-crowd-darkGray">days left</span>
         </div>
       </div>
       {/* progress bar */}
       <div className="h-4 w-full rounded-full bg-gray-200 lg:h-3">
-        <div className="h-full w-[80%] rounded-full bg-crowd-moderateCyan"></div>
+        <div
+          style={{ width: `${backingProgress}%` }}
+          className="h-full rounded-full bg-crowd-moderateCyan"
+        ></div>
       </div>
     </Card>
   );
